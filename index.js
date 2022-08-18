0400e247392e1a92cf3bdb3b94b5295e22570543
Pavel-Troinich <tr.pav.ar@gmail.com>
2022-08-18 22:17:32 +0300

    feat: add greeting

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// swow time and date
+
 function showDate() {
   const dates = document.querySelector('.date');
   const date = new Date();
   const options = {weekday: 'long', month: 'long', day: 'numeric', timeZoneName: 'short'};
-  const currentDate = date.toLocaleDateString('ru-Ru', options);
+  const currentDate = date.toLocaleDateString('en-En', options);
   dates.textContent = currentDate;
 }
 
@@ -13,8 +15,46 @@ function showTime() {
   time.textContent = currentTime;
 
   showDate()
+  showGreeting();
 
   setTimeout(showTime, 1000);
 };
 
 showTime();
+
+// show greeting
+
+function showGreeting() {
+  const greeting = document.querySelector('.greeting');
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay = '';
+  if (hours >= 0 && hours < 6) timeOfDay = 'night';
+  else if (hours >= 6 && hours <= 12) timeOfDay = 'morning';
+  else if (hours >= 12 && hours < 18) timeOfDay = 'day';
+  else if (hours >= 18 && hours < 24) timeOfDay = 'evening';
+
+  greeting.textContent = `Good ${timeOfDay}`;
+};
+
+// get Name
+const name = document.querySelector('.name');
+
+function setLocalItem() {
+  localStorage.setItem('name', name.value);
+};
+
+function getLocalItem() {
+  if(localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+  else {
+    name.value = '!';
+  }
+};
+
+window.addEventListener("beforeunload", setLocalItem);
+window.addEventListener("load", getLocalItem);
+
+
+
